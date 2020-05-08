87ed1bfe2349885a7fb179a7e8ae9ba82da9864a
vivi <15961591506@163.com>
2020-05-09 06:11:31 +0800

    5.9

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    entry: './src/component.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
     },
 
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Pomelo',
-        template: 'src/assets/index.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'pOMELO',
+            template: 'src/assets/index.html'//以template的模版生成新的html网页
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
+    ],
+
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['to-string-loader', 'css-loader'],
-                
+                //use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '/public/path/to/',
+                        },
+                    },
+                    'css-loader',
+                ],
             },
         ],
     },
