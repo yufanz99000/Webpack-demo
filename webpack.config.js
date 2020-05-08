dc8ae8355023af75e48ad65b2c979edea8db75b3
vivi <15961591506@163.com>
2020-05-08 21:27:43 +0800

    css?

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Pomelo',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['to-string-loader', 'css-loader'],
+                
+            },
+        ],
+    },
 };
+
