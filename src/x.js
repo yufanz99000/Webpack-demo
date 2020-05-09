8e46ab2f5db6837c245d7e6d5878abc4e69a3274
vivi <15961591506@163.com>
2020-05-09 09:39:53 +0800

    submit1

--- src/x.js
+++ src/x.js
@@ -3,3 +3,19 @@ import './x.scss'
 import './y.less'
 
 export default 'xxx'
+
+const div = document.getElementById('app')
+div.innerHTML = `hello,i am lazy`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick =() => {
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log('模块引用错误');
+    })
+}
+div.appendChild(button)
